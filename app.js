bb5da3260892dd77dec913151ce247bfbc6045ea
David Crandell <david@onholdwizard.com>
2021-09-10 17:34:50 -0500

    frogbot - a demo discord bot in nodejs for my son

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', msg => {
+	try{
+		switch (msg.content) {
+			case "!frog":
+				const embed3 = new MessageEmbed()
+					.setTitle("FROGGY TOAD!")
+					.setColor("blurple")
+					.setAuthor("Frog")
+					.setImage("https://i.imgur.com/VhmV9a8.jpg")
+				msg.channel.send(embed3)
+				.catch(console.error);
+				break;
+			}
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+try {
+	client.login(process.env.CLIENT_TOKEN);
+} catch(err) {
+	console.log(err);
+}
